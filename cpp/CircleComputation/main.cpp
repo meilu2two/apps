10d668d7c354be4fca70f26cd8ce9e1cc068e899
meilutwo <meilu@mail.de>
2015-08-20 22:31:29 +0200

    add code to stream circle data to files

--- cpp/CircleComputation/main.cpp
+++ cpp/CircleComputation/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <string>
 #include "point.h"
 #include "circle.h"
 
@@ -6,16 +10,30 @@ using namespace std;
 
 int main()
 {
-    point p1( 1, 1 );
-    circle c1( p1, 2 );
-    circle c2( 3, 3, 4 );
+    point a_p1( 1, 1 );
+    circle a_c1( a_p1, 2 );
+    circle a_c2( 3, 3, 4 );
 
-    p1.printcoordinates();
-    c1.printcoordinates();
-    c2.printcoordinates();
+    a_p1.printcoordinates();
+    a_c1.printcoordinates();
+    a_c2.printcoordinates();
 
     // output circle data by stream operator
-    cout << c1 << c2;
+    cout << a_c1 << a_c2;
+
+    // stream circle data to a file
+    int a_fileIndex( 0 );
+    while( 3 > a_fileIndex ) {
+        stringstream a_filename;
+        a_filename << "logfile" << setw(5) << setfill('0') << a_fileIndex++ << ".txt";
+        // ofstream a_outputFile;
+        // a_outputFile.open( a_filename.str().c_str() );
+        // if ( a_outputFile ) {
+        //     a_outputFile << a_c1 << a_c2;
+        //     a_outputFile.close();
+        // }
+        cout << a_filename.str() << endl;
+    }
 
     return 0;
 }
