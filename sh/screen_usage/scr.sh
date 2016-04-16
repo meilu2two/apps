931ff069cb7528ef44a5d0ac562257f4fd08cae4
meilu2two <meilu@mail.de>
2016-04-16 22:56:46 +0200

    arrange lightweight script scr.sh more beautiful

--- sh/screen_usage/scr.sh
+++ sh/screen_usage/scr.sh
@@ -1,19 +1,32 @@
 #!/bin/sh
 
+# lightweight all in one script to manage app in a screen session
+app_name="endless_echo.sh"
+session_name="my_app"
+echo "Lightweight screen manager for $session_name"
+
+#first command line argument is the command
 command=$1
 echo "Command: $command"
 
 case "$command" in
-  boot) /usr/bin/screen -dmS my_app /home/meilu/git/apps/sh/screen_usage/endless_echo.sh
+  boot) /usr/bin/screen -dmS $session_name /home/meilu/git/apps/sh/screen_usage/$app_name
   ;;
-  start) screen -dmS my_app ./endless_echo.sh
+  start) screen -dmS $session_name ./$app_name
   ;;
-  reattach) screen -rx my_app
+  reattach) screen -rx $session_name
   ;;
-  quit) screen -X -S my_app quit
+  quit) screen -X -S $session_name quit
   ;;
   list) screen -ls
   ;;
-  *) echo "default"
+  --help) echo "The following commands are provided:"
+     echo "start    ... start $session_name in screen session"
+     echo "reattach ... reattach screen session $session_name in multi display mode"
+     echo "quit     ... quit screen session $session_name"
+     echo "list     ... list all running screen sessions"
+  ;;
+  *) echo "unknown command: $command"
+     echo "Try '$0 --help' for more information"
   ;;
 esac
