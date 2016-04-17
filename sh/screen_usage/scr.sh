b56b02c3b25905247f49d88a87526c1f93f46c2c
meilu2two <meilu@mail.de>
2016-04-17 21:13:57 +0200

    Prevent starting multiple sessions with same name by using 'start' command

--- sh/screen_usage/scr.sh
+++ sh/screen_usage/scr.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-
 # lightweight all in one script to manage app in a screen session
+
 app_name="endless_echo.sh"
 session_name="my_app"
-echo "Lightweight screen manager for $session_name"
+echo "Screen manager for app '$app_name' alias '$session_name'"
 
-#first command line argument is the command
+# first command line argument is the command
 command=$1
 echo "Command: $command"
 
@@ -14,19 +14,26 @@ case "$command" in
         # For automatic start at boot add a line like the following to rc.local
         # /bin/su - username -c "/absolute/path/to/this/script/name_of_this_script.sh boot"
   ;;
-  start) screen -dmS $session_name ./$app_name
+  start) screen -ls | grep -q $session_name # grep for screen session with this name
+         if [ $? -gt 0 ] # is there already a screen session with this name
+           then
+             screen -dmS $session_name ./$app_name
+           else
+             echo "Error: A screen session with name '$session_name' is already running!"
+         fi
+         screen -ls
   ;;
-  reattach) screen -rx $session_name
+  reattach | reat) screen -rx $session_name
   ;;
   quit) screen -X -S $session_name quit
   ;;
   list) screen -ls
   ;;
-  --help) echo "The following commands are provided:"
-          echo "start    ... start $session_name in screen session"
-          echo "reattach ... reattach screen session $session_name in multi display mode"
-          echo "quit     ... quit screen session $session_name"
-          echo "list     ... list all running screen sessions"
+  --help | -h) echo "The following commands are provided:"
+               echo "start      ... start $session_name in screen session"
+               echo "reat(tach) ... reattach screen session $session_name in multi display mode"
+               echo "quit       ... quit screen session $session_name"
+               echo "list       ... list all running screen sessions"
   ;;
   *) echo "unknown command: $command"
      echo "Try '$0 --help' for more information"
